670b90b7d5afa5ad1e220737182a5a17625ab99c
baiyitao <tud46571@temple.edu>
2017-06-19 01:08:48 -0400

    0.0

---
+++ platforms/android/assets/www/js/user.js
@@ -0,0 +1,112 @@
+angular.module('starter.controllers')
+  .controller('UserCtrl', function($scope, $rootScope, $state,$http) {
+
+    if($rootScope.isLogin == undefined){
+      $rootScope.isLogin = false;
+    }
+    
+    $rootScope.user = {};
+
+    $scope.userInfo = {};
+
+    $scope.login = function(userInfo) {
+      console.log("login!");
+      console.log(userInfo);
+
+      //here call api, post user name and password、
+      $http({
+        method: "post",
+         url: 'http://sciencetap.us/tao/app/login.php',
+        data: userInfo
+      }).then(function(response) {
+
+        if(response.data.success == true){
+          $rootScope.isLogin = true;
+          $rootScope.user.firstname = response.data.data.firstname;
+          $rootScope.user.lastname = response.data.data.lastname;
+          $rootScope.user.id = response.data.data.id;
+          $rootScope.user.email = userInfo.email;
+          //if return yes, do/ show associate project-site(later)
+        } else {
+          $scope.error = response.data.error;
+          alert("login fail");
+        }
+
+
+      }, function(response) {
+        //else alert user
+        $scope.data = response.data || 'Request failed';
+        $scope.status = response.status;
+      });
+
+
+
+    }
+
+    $scope.logout = function() {
+      console.log("logout!");
+
+      $rootScope.user = {}
+      $rootScope.isLogin = false;
+    }
+
+    $scope.register = function() {
+      $state.go('tab.register');
+    }
+
+  })
+
+  .controller('ForgetPasswordCtrl', function($scope, $rootScope) {
+
+  })
+
+
+  .controller('RegisterCtrl', function($scope, $rootScope,$http,$state) {
+
+    $scope.formData = {};
+    $rootScope.user = {};
+    $scope.register = function(registerInfo) {
+      console.log("register!");
+      console.log(registerInfo);
+
+      if(registerInfo.password != registerInfo.password_c){
+        alert("password did not match");
+      } else {
+
+        $http({
+          method: "post",
+           url: 'http://sciencetap.us/tao/app/registerUser.php',
+          data: registerInfo
+        }).then(function(response) {
+
+          if(response.data.success == true){
+            alert("register success")
+            $rootScope.isLogin = true;
+            $rootScope.user.firstname = response.data.data.firstname;
+            $rootScope.user.lastname = response.data.data.lastname;
+            $rootScope.user.id = response.data.data.id;
+            $rootScope.user.email = registerInfo.email;
+            $state.go('tab.account');
+            //if return yes, do/ show associate project-site(later)
+          } else {
+            $scope.error = response.data.error;
+            alert("register fail");
+          }
+
+
+        }, function(response) {
+          //else alert user
+          alert("register fail");
+          $scope.data = response.data || 'Request failed';
+          $scope.status = response.status;
+        });
+        // here call api, post user name and password. register
+        // if return yes,
+        //   //auto login
+        //
+        // if no, stay here, tell user error
+      }
+
+
+    }
+  });
---
+++ www/js/manage.js
@@ -0,0 +1,70 @@
+angular.module('starter.controllers')
+.controller('ManageCtrl', function($scope,$rootScope,$state,$http) {
+
+  if(  $rootScope.isLogin == false || $rootScope.isLogin == undefined ){
+    alert("please login to see more");
+    $state.go('tab.account')
+  } else {
+    var data = {
+      "user_id" :  $rootScope.user.id
+    }
+    var request = $http({
+      method: "post",
+      url: 'http://sciencetap.us/tao/app/getSubmission.php',
+      data: data
+    })
+
+    request.success(function(data) {
+      $scope.showSub = true;
+      $scope.submission = data.data;
+      console.log($scope.submission);
+    })
+  }
+    $scope.gotoSubmission = function (id) {
+      //var test = 1234567;
+      var identity = {
+        "submission_id": id
+      }
+      $state.go('tab.manage-form', { data: identity });
+    }
+
+
+
+
+
+
+
+
+
+})
+
+.controller('ManageFormCtrl', function($scope,$rootScope,$state,$stateParams,$http) {
+
+  $scope.form = {
+    "id" : 1
+  }
+
+  var submission_id = $stateParams.data.submission_id[0];
+  console.log(submission_id);
+
+  $scope.submission_data ={};
+
+  var data = {
+    "submission_id" :  submission_id
+  }
+
+  var request = $http({
+    method: "post",
+    url: 'http://sciencetap.us/tao/app/getSubmitData.php',
+    data: data
+  })
+
+  request.success(function(data) {
+    $scope.showSub = true;
+    $scope.submission_data = data.data;
+    console.log($scope.submission_data);
+  })
+
+
+
+})
